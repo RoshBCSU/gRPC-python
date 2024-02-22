29e874830c543598388290b29f9b3177bbb927cd
RoshBCSU <RoshBCSU>
2024-02-22 05:52:29 -0500

    commit

--- examples/python/helloworld/greeter_client.py
+++ examples/python/helloworld/greeter_client.py
@@ -21,17 +21,33 @@ import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
 
+def call_both_methods():
+    # Create a channel to connect to the server
+    channel = grpc.insecure_channel("196.168.56.1:50051")
+   
+    # Create a gRPC stub using the channel
+    stub = helloworld_pb2_grpc.GreeterStub(channel)
+
+    # Call the SayHello method
+    response_hello = stub.SayHello(helloworld_pb2.HelloRequest(name="you"))
+    print("Greeter client received (SayHello): " + response_hello.message)
+
+    # Call the SayHelloAgain method
+    response_hello_again = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name="you"))
+    print("Greeter client received (SayHelloAgain): " + response_hello_again.message)
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
     print("Will try to greet world ...")
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel("196.168.56.1:50051") as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name="you"))
     print("Greeter client received: " + response.message)
-
+    response = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
+    print("Greeter client received: " + response.message)
+    call_both_method()
 
 if __name__ == "__main__":
     logging.basicConfig()
